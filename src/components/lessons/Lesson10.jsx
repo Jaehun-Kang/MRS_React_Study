9c57f0bd8c70d6bb56501395ae14b250c82be478
jyiyoon4 <234168507+jyiyoon4@users.noreply.github.com>
2026-04-14 15:43:25 +0900

    10강

--- src/components/lessons/Lesson10.jsx
+++ src/components/lessons/Lesson10.jsx
@@ -1,6 +1,6 @@
 import '../../styles/lesson.css';
 import CodeBlock from '../CodeBlock';
-import Foldable from "../Foldable";
+import Foldable from '../Foldable';
 
 const Lesson10 = {
   name: '10. State 기초',
@@ -8,22 +8,102 @@ const Lesson10 = {
   content: (
     <div className="lesson-content">
       <h2>State 기초</h2>
-      <p>내용</p>
-      <CodeBlock
-        filename="MyComponent.jsx"
-        language="jsx"
-        code={`code`}
-      />
-      <Foldable title="폴더제목">
-        <p>내용</p>
+
+      <Foldable title="시작 코드">
         <CodeBlock
-          filename="MyComponent.jsx"
+          filename="App.jsx"
           language="jsx"
-          code={`code`}
+          code={`import './App.css'
+import { useState } from 'react'
+
+
+function App() {
+
+
+  const [count, setCount] = useState(0)
+
+
+  console.log(useState(0))
+
+
+  return (
+    <>
+    {/*코드필요*/}
+    </>
+  )
+}
+
+
+export default App`}
         />
       </Foldable>
-    </div>
+
+      <h3>리렌더링이란?</h3>
+      <p>
+        사용자 화면에 View(내용)를 보여 주는 것을 렌더링이라고 한다.
+        <br />
+        리액트는 리렌더링이 발생할 때마다 이전의 가상 DOM과 새 가상 DOM을
+        비교하여 바뀐 부분만 골라내어 업데이트한다. 이 덕에, 웹페이지가 새로
+        고침되지 않은 채 내용만 바뀌는 걸 확인할 수 있다.
+      </p>
+      <br />
+      <CodeBlock
+        filename="App.jsx"
+        language="jsx"
+        code={`import './App.css'
+import { useState } from 'react'
+
+function App() {
+
+{/*코드필요*/}
+
+  }
+
+  return (
+    <>
+      <button onClick={togglePinned}>
+        {isPinned && '📌'} Pinn This!
+      </button>
+    </>
   )
+}
+
+export default App`}
+      ></CodeBlock>
+
+      <CodeBlock
+        filename="App.jsx"
+        language="jsx"
+        code={`
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+
+  {/*코드 필요*/}
+
+  return (
+    <>
+      <h3>Todo List</h3>
+      <ul>
+        {/*코드 필요*/}
+      </ul>
+      <p>Typing: {newTodo}</p>
+      <input type='text' value={newTodo}
+        onChange={
+          (e) => setNewTodo(e.target.value)}/>
+      <button onClick={() => addTodo(newTodo)}>
+        Add New Task
+      </button>
+    </>
+  )
+}
+
+export default App
+  `}
+      ></CodeBlock>
+    </div>
+  ),
 };
 
 export default Lesson10;
