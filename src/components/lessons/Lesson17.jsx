4a23e43992dc98920ae011c11b0e2adeeed9cffe
jyiyoon4 <234168507+jyiyoon4@users.noreply.github.com>
2026-08-01 12:01:05 +0900

    17,18

--- src/components/lessons/Lesson17.jsx
+++ src/components/lessons/Lesson17.jsx
@@ -1,6 +1,6 @@
 import '../../styles/lesson.css';
 import CodeBlock from '../CodeBlock';
-import Foldable from "../Foldable";
+import Foldable from '../Foldable';
 
 const Lesson17 = {
   name: '17. Context',
@@ -8,22 +8,228 @@ const Lesson17 = {
   content: (
     <div className="lesson-content">
       <h2>Context</h2>
-      <p>내용</p>
-      <CodeBlock
-        filename="MyComponent.jsx"
-        language="jsx"
-        code={`code`}
-      />
-      <Foldable title="폴더제목">
-        <p>내용</p>
+      <p>
+        Props는 부모에서 특정 자식에게 데이터를 전달할 때, 중간에 있는 모든
+        단계들을 거쳐 전달해야 하는 방식이다.
+        <br />
+        Context는 중간 컴포넌트를 거치지 않고 필요한 컴포넌트에만 연결해 직접
+        데이터를 전달할 수 있어, 불필요한 Props 전달을 줄이고 유지보수를 쉽게
+        해준다.
+      </p>
+      <Foldable title="시작 코드">
         <CodeBlock
-          filename="MyComponent.jsx"
+          filename="App.jsx"
           language="jsx"
-          code={`code`}
+          code={`import './App.css'
+import { useState }
+  from 'react'
+import Child1
+  from './Child1'
+
+function App() {
+  const [count, setCount]
+    = useState(0)
+
+  return (
+    <div>
+      <h2>App</h2>
+      <Child1
+        count={count} setCount={setCount} 
         />
-      </Foldable>
     </div>
   )
+}
+
+export default App`}
+        />
+
+        <CodeBlock
+          filename="Child1.jsx"
+          language="jsx"
+          code={`import Child2
+  from './Child2'
+
+function Child1(
+  { count, setCount }
+) {
+  return (
+    <div>
+      <h2>Child1</h2>
+      <Child2
+        count={count}
+        setCount={
+          setCount
+        } 
+      />
+    </div>
+  )
+}
+
+export default Child1`}
+        />
+
+        <CodeBlock
+          filename="Child2.jsx"
+          language="jsx"
+          code={`function Child2(
+  { count, setCount }
+) {
+  return (
+    <div>
+      <h2>Child2</h2>
+      <p>
+      Count: {count}
+      </p>
+      <button onClick={
+        () => setCount(
+          count + 1
+          )}>
+          Increase
+          </button>
+          </div>
+          )
+          }
+          
+          export default Child2`}
+        />
+      </Foldable>
+
+      <h3>Provider</h3>
+      <p>
+        Provider는 Context의 데이터를 하위 컴포넌트에 전달하는 컴포넌트이다.
+        <br /> 이전에는 'Context.Provider'를 통해 사용했지만, React 19부터는
+        '.Provider'를 붙이지 않고 사용할 수 있다.
+      </p>
+      <CodeBlock
+        filename="CountContext.jsx"
+        language="jsx"
+        code={`import { createContext, useState } from 'react'
+          
+          const CountContext = createContext()
+          
+          //코드 필요
+          
+          export { CountContext, CountProvider }`}
+      />
+
+      <Foldable title="CountContext Child">
+        <CodeBlock
+          filename="App.jsx"
+          language="jsx"
+          code={`import './App.css'
+  import { CountProvider } from './contexts/CountContext'
+  import Child1 from './Child1'
+  
+  function App() {
+    return (
+      //코드 필요
+      )
+      }
+      
+      export default App`}
+        />
+
+        <CodeBlock
+          filename="Child1.jsx"
+          language="jsx"
+          code={`import Child2 from './Child2'
+
+function Child1() {
+  return (
+    <div>
+    <h2>Child</h2>
+    <Child2 />
+    </div>
+    )
+}
+
+export default Child1`}
+        />
+
+        <CodeBlock
+          filename="Child2.jsx"
+          language="jsx"
+          code={`import { useContext } from 'react'
+import { CountContext } from './contexts/CountContext'
+
+function Child2() {
+  //코드 필요
+}
+
+export default Child2
+`}
+        />
+      </Foldable>
+      <p>
+        하나의 Context에 많은 컴포넌트를 포함하면 값이 변경될 때 불필요한
+        리렌더링이 발생할 수 있다. 따라서 용도별로 Context를 분리하면, 특정
+        Context의 변경이 다른 Context를 사용하는 컴포넌트에 영향을 주지 않는다.
+      </p>
+
+      <Foldable title="ToggleContext, Child">
+        <CodeBlock
+          filename="ToggleContext.jsx"
+          language="jsx"
+          code={`import { createContext, useState } from 'react'
+          
+          const ToggleContext = createContext()
+          
+          function ToggleProvider({ children }) {
+            //코드 필요
+              }
+              
+              export { ToggleContext, ToggleProvider }
+              `}
+        />
+
+        <CodeBlock
+          filename="Child3.jsx"
+          language="jsx"
+          code={`import { useContext } from 'react'
+import Child4 from './Child4'
+import { ToggleContext } from './contexts/ToggleContext'
+
+function Child3() {
+  //코드 필요
+
+  return (
+    <div>
+      <h2>Child3</h2>
+      //코드 필요
+    </div>
+  )
+}
+
+export default Child3
+`}
+        />
+
+        <CodeBlock
+          filename="Child4.jsx"
+          language="jsx"
+          code={`import { useContext } from 'react'
+import { ToggleContext } from './contexts/ToggleContext'
+
+function Child4() {
+  //코드 필요
+
+  return (
+    <div>
+      <h2>Child4</h2>
+      <button onClick={toggle}>Toggle</button>
+    </div>
+  )
+}
+
+export default Child4`}
+        />
+      </Foldable>
+      <p>
+        이처럼, useContext 훅을 사용하면 필요한 Context의 데이터만 가져와 사용할
+        수 있다.
+      </p>
+    </div>
+  ),
 };
 
 export default Lesson17;
--- src/components/lessons/Lesson18.jsx
+++ src/components/lessons/Lesson18.jsx
@@ -1,6 +1,6 @@
 import '../../styles/lesson.css';
 import CodeBlock from '../CodeBlock';
-import Foldable from "../Foldable";
+import Foldable from '../Foldable';
 
 const Lesson18 = {
   name: '18. 라우팅',
@@ -8,22 +8,252 @@ const Lesson18 = {
   content: (
     <div className="lesson-content">
       <h2>라우팅</h2>
-      <p>내용</p>
+      <p>
+        주어진 URL에 따라 페이지 및 컨텐츠를 제공하는 방식이다.
+        <br />
+        특정 링크를 클릭했을 때 해당하는 페이지로 이동하도록 하는 등의 용도로
+        사용된다.
+      </p>
+      <p>
+        a 태그는 페이지를 이동할 때마다 서버에서 새로운 HTML 문서를 받아온다.
+        반면 React의 싱글 페이지 애플리케이션은 처음 로드된 페이지를 유지한 채
+        필요한 화면만 변경하여 페이지를 전환한다.
+      </p>
       <CodeBlock
-        filename="MyComponent.jsx"
+        filename="라이브러리 설치용"
         language="jsx"
-        code={`code`}
+        code={`react-router-dom`}
       />
-      <Foldable title="폴더제목">
-        <p>내용</p>
+      <Foldable title="시작 코드">
         <CodeBlock
-          filename="MyComponent.jsx"
+          filename="App.css"
+          language="css"
+          code={`#root {
+  margin: 0 auto;
+}
+
+#root button {
+  margin: 4px;
+  background-color: #f0f0f0;
+}
+
+#root nav > * {
+  margin: 0 6px;
+}`}
+        />
+        <CodeBlock
+          filename="main.jsx"
           language="jsx"
-          code={`code`}
+          code={`import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)`}
+        />
+
+        <CodeBlock
+          filename="Home.jsx"
+          language="jsx"
+          code={`const Home = () => (
+  <>
+    <h2>Home</h2>
+    <p>
+      Welcome to Web Dev Class!
+    </p>
+  </>
+)
+
+export default Home`}
+        />
+
+        <CodeBlock
+          filename="About.jsx"
+          language="jsx"
+          code={`const About = () => (
+  <>
+    <h2>About</h2>
+    <ul>
+      {['HTML', 'CSS', 'JavaScript']
+        .map((item, idx) => (
+          <li key={idx}>{item}</li>
+        )
+      )}
+    </ul>
+  </>
+)
+
+export default About`}
+        />
+
+        <CodeBlock
+          filename="Contact.jsx"
+          language="jsx"
+          code={`const Contact = () => (
+  <>
+    <h2>Contact</h2>
+    <address>
+      email: 
+      <a href="mailto:yalco@yalco.kr">
+          yalco@yalco.kr
+      </a>
+    </address>
+  </>
+)
+
+export default Contact`}
+        />
+
+        <CodeBlock
+          filename="App.jsx"
+          language="jsx"
+          code={`import './App.css'
+import { BrowserRouter, Routes, Route, Link }
+  from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <>
+      //코드 필요
+    </>
+  )
+}
+
+export default App`}
         />
       </Foldable>
-    </div>
+
+      <h3> Routes, Route</h3>
+      <p>
+        Routes는 다수의 Root 요소를 포함할 수 있으며, 현재 URL과 path가 일치하는
+        Route 하나만 렌더링한다. 각 Route는 path로 URL 경로를, element로
+        렌더링할 컴포넌트를 지정한다.
+      </p>
+
+      <h3> Link</h3>
+      <p>
+        Link는 to 속성에 지정한 경로로 URL을 변경하여 페이지를 이동하는
+        컴포넌트이다.
+        <br />a 태그와 달리 서버에 새로 요청을 보내지 않고, 페이지를
+        새로고침하지 않은 채 URL과 히스토리만 프로그래밍적으로 수정하여 해당
+        Route를 렌더링한다.
+      </p>
+
+      <h3> useNavigate</h3>
+      <p>
+        useNavigate는 프로그래밍적으로 경로를 변경할 수 있는 훅이다. 반환된
+        함수에 이동할 경로를 전달하면 해당 페이지로 이동하며, Link처럼 미리
+        경로를 지정하는 방식이 아닌, 동적으로 경로를 변경해야 하는 경우 유용하게
+        사용할 수 있다.
+      </p>
+
+      <CodeBlock
+        filename="App.jsx"
+        language="jsx"
+        code={`coimport './App.css'
+import { Routes, Route, useParams, useLocation }
+  from 'react-router-dom'
+import React, { useEffect } from 'react'
+
+function Home() {
+  //코드 필요
+
+  return <h1>Home Page</h1>
+}
+
+function User() {
+  const { id } = useParams()
+  const location = useLocation()
+
+  useEffect(() => {
+    console.log('Current Path:', location.pathname)
+    console.log('URL Parameter (id):', id)
+  }, [id, location])
+
+  return <h1>User ID: {id}</h1>
+}
+
+function Search() {
+  //코드 필요
+
+  useEffect(() => {
+    console.log('Current Path:', location.pathname)
+    console.log(
+      'Query Parameter (keyword):', keyword
+    )}, [keyword, location])
+
+  return <h1>Search Keyword: {keyword}</h1>
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/user/:id' element={<User />} />
+      <Route path='/search' element={<Search />} />
+    </Routes>
   )
+}
+
+export default Appde`}
+      />
+
+      <h3> useLocation</h3>
+      <p>
+        현재 URL의 위치 정보를 담은 Location 객체를 반환하는 훅이다. pathname,
+        search 등의 정보를 사용할 수 있으며, URL이 변경되면 함께 업데이트된다.
+      </p>
+
+      <h3> useParams</h3>
+      <p>
+        URL의 동적 파라미터를 가져오는 훅이다. :id와 같이 정의된 경로의 값을
+        객체 형태로 반환한다.
+      </p>
+
+      <h3>잘못된 URL</h3>
+      <p>
+        등록되지 않은 URL에 접근하는 경우를 처리하기 위해 path="*"를 사용한다.
+        *는 다른 Route와 매칭되지 않는 모든 경로를 처리하며, 오류 페이지를
+        렌더링할 수 있다.
+      </p>
+
+      <CodeBlock
+        filename="App.jsx"
+        language="jsx"
+        code={`import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/wrong-path">Wrong Path</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h2>Home Page</h2>} />
+        <Route path="/about" element={<h2>About Page</h2>} />
+        <Route path="*" element={<h2>404 - Page Not Found</h2>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App`}
+      />
+
+      <CodeBlock filename="MyComponent.jsx" language="jsx" code={`code`} />
+    </div>
+  ),
 };
 
 export default Lesson18;
